f53f046bd19df851f59a35727e5aaf93b52569f1
shajagraphics <shahbaziqbal753@gmail.com>
2020-12-05 15:20:51 +0500

    Added Pofile Image and Name or Roll Number on screen

--- lib/main.dart
+++ lib/main.dart
@@ -28,3 +28,47 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 6),
+            () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) =>DiceeApp ())));
+  } //<- Creates an object that fetches an image.
+  // var image = new Image(
+  //     image: AssetImage(
+  //         'asset/quiz.png'),
+  //     height:300);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          gradient:  LinearGradient(
+              begin: Alignment.topCenter,
+              colors: [
+                Colors.blueAccent,
+                Colors.grey,
+                Colors.blueAccent,
+              ]
+          ),
+        ),
+
+        child: Column(
+          children: <Widget>[
+            Image(
+                image: AssetImage(
+                    'images/profile.jpg'),
+                height:300),
+            Text("Muhammad Shahbaz Iqbal ",style:TextStyle(fontSize: 30,color: Colors.blue)),
+            SizedBox(height: 5,),
+            Text(" sp17-bcs-051",style:TextStyle(fontSize: 30,color: Colors.blue)),
+
+          ],
+        )
+    );
+  }
+}
+
+
